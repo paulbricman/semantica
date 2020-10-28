e8c80cd0876c65fd6e5e8fa43fd7acf77424e8c3
paubric <paubric@gmail.com>
2020-10-28 19:15:50 +0100

    Update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from semantica import Semantica
 
-s = Semantica('./models/GoogleNews-vectors-negative300.bin', word_count=10000)
+s = Semantica('./models/GoogleNews-vectors-negative300.bin', word_count=100000)
 
 #print(*s.field([s.mix(["king", s.shift("man", "woman")]), "tree"]), sep='\n')
-#print(*s.field(s.span("male", "female")), sep='\n')
-print(s.field(s.model(["eat", "food"])))
+print(*s.field(s.span("car", "boat"), norm_concepts=False), sep='\n')
+#print(s.field(s.model(["eat", "food"])))
--- semantica.py
+++ semantica.py
@@ -11,6 +11,10 @@ class Semantica:
             model_path, binary=True, limit=word_count)
 
     def to_vector(self, concept, norm=True):
+        """Turn a key or vector into a vector.
+
+        If `concept` is a vector, return it. If it's a key, return the associated vector after normalizing it.
+        """
         if isinstance(concept, ndarray):
             return concept
         else:
@@ -19,45 +23,65 @@ class Semantica:
             else:
                 return self.c.get_vector(concept)
 
-    def field(self, concepts, norm=True):
+    def field(self, concepts, norm_concepts=True):
+        """Return semantic fields of given concepts.
+
+        Extract the most similar concepts for each element of `concepts`, and return them.
+        """
         fields = []
 
         if isinstance(concepts, (str, ndarray)):
             concepts = [concepts]
 
         for concept in concepts:
-            field = self.c.most_similar([self.to_vector(concept, norm=norm)])
+            field = self.c.most_similar([self.to_vector(concept, norm=norm_concepts)])
             field = [e[0] for e in field]
             fields += [field]
 
         return fields
 
-    def mix(self, concepts, norm=True):
+    def mix(self, concepts, norm_concepts=True, norm_result=True):
+        """Combine the meaning of multiple concepts.
+
+        Average the vectors associated with the given concepts and return the normalized result.
+        """
         concept_vectors = []
 
         for concept in concepts:
-            concept_vectors += [self.to_vector(concept, norm=norm)]
+            concept_vectors += [self.to_vector(concept, norm=norm_concepts)]
 
-        mix = matutils.unitvec(
-            array(concept_vectors).mean(axis=0)).astype(float32)
+        mix = array(concept_vectors).mean(axis=0).astype(float32)
+
+        if norm_result:
+            mix = matutils.unitvec(mix)
 
         return mix
 
-    def shift(self, source, target, norm=True):
-        source_vector = -self.to_vector(source, norm=norm)
-        target_vector = self.to_vector(target, norm=norm)
+    def shift(self, source, target, norm_concepts=False, norm_result=True):
+        """Return a vector which encodes a meaningful semantic shift.
 
-        shift = matutils.unitvec(
-            array([source_vector, target_vector]).mean(axis=0)).astype(float32)
+        Compute the difference between a source and target vector, and return the normalized result.
+        """
+        source_vector = -1 * self.to_vector(source, norm=norm_concepts)
+        target_vector = self.to_vector(target, norm=norm_concepts)
+
+        shift = array([source_vector, target_vector]).mean(axis=0).astype(float32)
+
+        if norm_result:
+            shift = matutils.unitvec(shift)
 
         return shift
 
-    def span(self, start, end, steps=5):
+    def span(self, start, end, steps=10, norm_concepts=False, norm_shift_result=False, norm_result=False, norm_mix_concepts=True):
+        """Return an interpolation of the semantic space between two concepts.
+
+        
+        """
         step_vectors = []
-        shift = self.shift(start, end)
+        shift = self.shift(start, end, norm_concepts=norm_concepts, norm_result=norm_shift_result)
 
         for step in range(1, steps + 1):
-            step_vector = self.mix([start, shift * (1 / steps) * step])
+            step_vector = self.mix([start, shift * (1 / steps) * step], norm_result=norm_result, norm_concepts=norm_mix_concepts)
             step_vectors += [step_vector]
 
         return step_vectors
@@ -72,22 +96,26 @@ class Semantica:
             new_leaf_concepts = []
 
             new_root_vector = self.c.vectors[i]
-            new_leaf_vectors = [self.mix([new_root_vector, skeleton[j]]) for j in range(len(skeleton))]
+            new_leaf_vectors = [
+                self.mix([new_root_vector, skeleton[j]]) for j in range(len(skeleton))]
 
             new_root_concept = self.c.similar_by_vector(new_root_vector)[0][0]
             new_leaf_concepts = []
 
             for new_leaf_vector in new_leaf_vectors:
-                new_leaf_concept = [e[0] for e in self.c.similar_by_vector(new_leaf_vector) if e[0] not in [new_root_concept, *new_leaf_concepts]][0]
+                new_leaf_concept = [e[0] for e in self.c.similar_by_vector(
+                    new_leaf_vector) if e[0] not in [new_root_concept, *new_leaf_concepts]][0]
                 new_leaf_concepts += [new_leaf_concept]
 
             for i in range(len(new_leaf_vectors)):
-                match_score += [dot(self.shift(new_root_concept, new_leaf_concepts[i]), skeleton[i])]
+                match_score += [dot(self.shift(new_root_concept,
+                                               new_leaf_concepts[i]), skeleton[i])]
 
             for i in range(len(new_leaf_vectors)):
-                match_score += [dot(matutils.unitvec(new_leaf_vectors[i]), matutils.unitvec(self.to_vector(new_leaf_concepts[i])))]
+                match_score += [dot(matutils.unitvec(new_leaf_vectors[i]),
+                                    matutils.unitvec(self.to_vector(new_leaf_concepts[i])))]
 
-            #if "man" in [e[0] for e in self.c.similar_by_vector(new_root)]:
+            # if "man" in [e[0] for e in self.c.similar_by_vector(new_root)]:
             #    print('---', [e for e in [self.c.similar_by_vector(e) for e in [new_root] + new_leafs]], sep='\n')
 
             match_score = mean(match_score)
