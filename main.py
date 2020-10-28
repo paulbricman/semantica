44ea75f691e4ca4e7151bfb07fcd9dce409263f4
paubric <paubric@gmail.com>
2020-10-28 10:43:18 +0100

    Implement to_vector

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from semantica import Semantica
 
 s = Semantica('./models/GoogleNews-vectors-negative300.bin', word_count=100000)
 
-print(s.field(["king", s.shift("man", "woman")]))
+print(s.field(s.mix(["king", s.shift("man", "woman")])))
--- semantica.py
+++ semantica.py
@@ -4,21 +4,43 @@ from gensim import matutils
 import numpy as np
 from numpy import ndarray, float32, array
 
+
 class Semantica:
     def __init__(self, model_path, word_count=1000000):
         self.c = gensim.models.KeyedVectors.load_word2vec_format(
             model_path, binary=True, limit=word_count)
 
-    def field(self, concept):
-        concepts = self.c.most_similar(concept)
-        concepts = [e[0] for e in concepts]
+    def to_vector(self, concept, norm=False):
+        if isinstance(concept, ndarray):
+            return concept
+        else:
+            if norm:
+                return matutils.unitvec(self.c.get_vector(concept))
+            else:
+                return self.c.get_vector(concept)
 
-        return concepts
+    def field(self, concept):
+        field = self.c.most_similar([self.to_vector(concept, norm=True)])
+        field = [e[0] for e in field]
+
+        return field
+
+    def mix(self, concepts):
+        concept_vectors = []
+
+        for concept in concepts:
+            concept_vectors += [self.to_vector(concept, norm=True)]
+
+        mix = matutils.unitvec(
+            array(concept_vectors).mean(axis=0)).astype(float32)
+
+        return mix
 
     def shift(self, source, target):
-        source_vector = -self.c.get_vector(source)
-        target_vector = self.c.get_vector(target)
+        source_vector = -self.to_vector(source, norm=True)
+        target_vector = self.to_vector(target, norm=True)
 
-        shift = matutils.unitvec(array([source_vector, target_vector]).mean(axis=0)).astype(float32)
+        shift = matutils.unitvec(
+            array([source_vector, target_vector]).mean(axis=0)).astype(float32)
 
         return shift
