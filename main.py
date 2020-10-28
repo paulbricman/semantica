d90cd35cf676de292bb2afbfd94718ab45c954f4
paubric <paubric@gmail.com>
2020-10-28 13:27:15 +0100

    Implement sad models

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from semantica import Semantica
 
-s = Semantica('./models/GoogleNews-vectors-negative300.bin', word_count=1000000)
+s = Semantica('./models/GoogleNews-vectors-negative300.bin', word_count=10000)
 
-print(*s.field([s.mix(["king", s.shift("man", "woman")]), "tree"]), sep='\n')
-print(*s.field(s.span("male", "female")), sep='\n')
+#print(*s.field([s.mix(["king", s.shift("man", "woman")]), "tree"]), sep='\n')
+#print(*s.field(s.span("male", "female")), sep='\n')
+print(s.field(s.model(["eat", "food"])))
--- semantica.py
+++ semantica.py
@@ -2,7 +2,7 @@ import gensim
 from gensim import matutils
 
 import numpy as np
-from numpy import ndarray, float32, array
+from numpy import ndarray, float32, array, dot, mean
 
 
 class Semantica:
@@ -10,7 +10,7 @@ class Semantica:
         self.c = gensim.models.KeyedVectors.load_word2vec_format(
             model_path, binary=True, limit=word_count)
 
-    def to_vector(self, concept, norm=False):
+    def to_vector(self, concept, norm=True):
         if isinstance(concept, ndarray):
             return concept
         else:
@@ -19,24 +19,24 @@ class Semantica:
             else:
                 return self.c.get_vector(concept)
 
-    def field(self, concepts):
+    def field(self, concepts, norm=True):
         fields = []
 
         if isinstance(concepts, (str, ndarray)):
             concepts = [concepts]
 
         for concept in concepts:
-            field = self.c.most_similar([self.to_vector(concept, norm=True)])
+            field = self.c.most_similar([self.to_vector(concept, norm=norm)])
             field = [e[0] for e in field]
             fields += [field]
 
         return fields
 
-    def mix(self, concepts):
+    def mix(self, concepts, norm=True):
         concept_vectors = []
 
         for concept in concepts:
-            concept_vectors += [self.to_vector(concept, norm=True)]
+            concept_vectors += [self.to_vector(concept, norm=norm)]
 
         mix = matutils.unitvec(
             array(concept_vectors).mean(axis=0)).astype(float32)
@@ -62,3 +62,38 @@ class Semantica:
 
         return step_vectors
 
+    def model(self, model, match_threshold=0.6):
+        root = model[0]
+        skeleton = [self.shift(root, e) for e in model[1:]]
+        matches = []
+
+        for i in range(len(self.c.vectors)):
+            match_score = []
+            new_leaf_concepts = []
+
+            new_root_vector = self.c.vectors[i]
+            new_leaf_vectors = [self.mix([new_root_vector, skeleton[j]]) for j in range(len(skeleton))]
+
+            new_root_concept = self.c.similar_by_vector(new_root_vector)[0][0]
+            new_leaf_concepts = []
+
+            for new_leaf_vector in new_leaf_vectors:
+                new_leaf_concept = [e[0] for e in self.c.similar_by_vector(new_leaf_vector) if e[0] not in [new_root_concept, *new_leaf_concepts]][0]
+                new_leaf_concepts += [new_leaf_concept]
+
+            for i in range(len(new_leaf_vectors)):
+                match_score += [dot(self.shift(new_root_concept, new_leaf_concepts[i]), skeleton[i])]
+
+            for i in range(len(new_leaf_vectors)):
+                match_score += [dot(matutils.unitvec(new_leaf_vectors[i]), matutils.unitvec(self.to_vector(new_leaf_concepts[i])))]
+
+            #if "man" in [e[0] for e in self.c.similar_by_vector(new_root)]:
+            #    print('---', [e for e in [self.c.similar_by_vector(e) for e in [new_root] + new_leafs]], sep='\n')
+
+            match_score = mean(match_score)
+
+            if match_score > match_threshold:
+                match = [new_root_concept, *new_leaf_concepts]
+                #matches += [new_root, *new_leafs]
+                print('---', *self.field(match), sep='\n')
+                print(match_score)
