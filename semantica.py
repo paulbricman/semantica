eddf4e7c5b1e3681b9e50fda62cfe1b651b2f500
paubric <paubric@gmail.com>
2020-10-28 09:40:45 +0100

    Set up Semantica class with constructor

--- semantica.py
+++ semantica.py
@@ -1,2 +1,5 @@
 import gensim
 
+class Semantica:
+    def __init__(self, model_path, word_count=1000000):
+        self.w = gensim.models.KeyedVectors.load_word2vec_format(model_path, binary=True, limit=word_count)
